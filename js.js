c219d658199efab6dbb97bca5230c2bdc869e7fc
normuhamedov <normuhamedov22@gmail.com>
2023-03-02 10:45:03 +0500

    sahkdj

--- js.js
+++ js.js
@@ -1,47 +1,47 @@
-const apiKey = '5230a42f11a0dd71788f9e055a1f7249';
-const baseUrl = 'https://api.themoviedb.org/3/';
+const apiKeys = '5230a42f11a0dd71788f9e055a1f7249';
+const moviesUrl = 'https://api.themoviedb.org/3/';
 
-const imagesUrl = 'https://image.tmdb.org/t/p/w500';
+const imgUrl = 'https://image.tmdb.org/t/p/w500';
 
 
 const fragment = document.createDocumentFragment();
 
 
-const loader_container = document.querySelector('.loader-container');
+const loaderContainer = document.querySelector('.loader-container');
 const loader = document.createElement('div');
 loader.classList.add('loader');
 
-loader_container.appendChild(loader);
+loaderContainer.appendChild(loader);
 
 
 async function myAsyncFunction() {
 
-    const searchTerm = document.querySelector('#search').value;
+    const search = document.querySelector('#search').value;
 
     let url;
 
-    if (searchTerm) {
-        url = `${baseUrl}search/movie?api_key=${apiKey}&query=${searchTerm}`;
+    if (search) {
+        url = `${moviesUrl}search/movie?api_key=${apiKeys}&query=${search}`;
     } else {
-        url = `${baseUrl}discover/movie?api_key=${apiKey}`;
+        url = `${moviesUrl}discover/movie?api_key=${apiKeys}`;
     }
 
     try {
         const response = await fetch(url);
         const data = await response.json();
 
-        const movie_container = document.querySelector('.movie-container');
+        const movieContainer = document.querySelector('.movie-container');
 
-        movie_container.innerHTML = '';
+        movieContainer.innerHTML = '';
 
         data.results.map((movie) => {
             const movieCard = document.createElement('div');
             movieCard.classList.add('movie-card');
 
 
-            const movieImage = document.createElement('img');
-            movieImage.src = `${imagesUrl}${movie.backdrop_path}`;
-            movieImage.alt = movie.title;
+            const movieImg = document.createElement('img');
+            movieImg.src = `${imgUrl}${movie.backdrop_path}`;
+            movieImg.alt = movie.title;
 
             const movieTitle = document.createElement('h3');
             movieTitle.textContent = movie.title;
@@ -51,11 +51,11 @@ async function myAsyncFunction() {
 
 
 
-            fragment.appendChild(movieImage);
+            fragment.appendChild(movieImg);
             fragment.appendChild(movieTitle);
             fragment.appendChild(movieRelease);
             movieCard.appendChild(fragment);
-            movie_container.appendChild(movieCard);
+            movieContainer.appendChild(movieCard);
         })
 
     } catch (error) {
@@ -63,9 +63,9 @@ async function myAsyncFunction() {
     } finally {
         setTimeout(() => {
             
-            const loader = document.querySelector('.loader');
-            if (loader) {
-                loader_container.remove();
+            const kunfu = document.querySelector('.loader');
+            if (kunfu) {
+                loaderContainer.remove();
             }
         }, 2000);
     }
